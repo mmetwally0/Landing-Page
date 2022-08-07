83665b16c1980e8be2f51438944a1f8be6a13313
mmetwally0 <mohamed.saleh_a1208@pt.kfs.edu.eg>
2022-08-07 13:33:13 +0200

    Added required Functionality

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 const sections = document.querySelectorAll('section');
 const navLinks = document.querySelector('.navigation-links');
 
+
 // Create A document fragment to store the 'li' elements before appending them to the DOM
 const fragment = document.createDocumentFragment();
 
@@ -10,8 +11,9 @@ const fragment = document.createDocumentFragment();
 sections.forEach((section) => {
     // Creating a new li Element and setting its contents to the data-set from section.
     const li = document.createElement('li');
-    li.textContent = `${section.dataset.nav}`;
-    li.classList.add('nav-link');
+    li.textContent = `Section ${section.dataset.nav}`;
+    li.setAttribute('id', `${section.dataset.nav}`)
+    li.classList.add('nav-link')
 
     // Adding the Element to the fragment
     fragment.appendChild(li);
@@ -20,20 +22,45 @@ sections.forEach((section) => {
 // Adding the Navigation links to the DOM
 navLinks.appendChild(fragment);
 
+// Creating a click event listener
+navLinks.addEventListener('click', (event) => {
+    const clickedLink = `section-${event.target.id}`
+    event.preventDefault()
+    
+    // Scroll to the desired section
+    const element = document.querySelector(`#${clickedLink}`)
+    
+    element.scrollIntoView()
+})
 
 // Creating a scroll event listener
 document.addEventListener('scroll', (event) => {
-    // use inner height and this.scrollY to get the height scrolled.
-    const section = document.querySelector('#section-one')
-    const rect = section.getBoundingClientRect()
-    const link = document.querySelector('.nav-link')
-    // console.log(rect.top)
-    // console.log(rect.bottom)
+    // // use inner height and this.scrollY to get the height scrolled.
+    // const section = document.querySelector('#section-one')
+    // const rect = section.getBoundingClientRect()
+    // const link = document.querySelector('#one')
+    // // console.log(rect.top)
+    // // console.log(rect.bottom)
     
-    if ((0 - rect.height) <= rect.top && rect.top < 0) {
-        link.classList.add('active')
-    } else {
+    // if ((0 - rect.height) <= rect.top && rect.top < 1) {
+    //     link.classList.add('active')
+    // } else {
         
-        link.classList.remove('active')
-    }
+    //     link.classList.remove('active')
+    // }
+    //---------------------------------
+
+    // Loop Through Every section on page
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const navLinks = document.querySelectorAll('nav-link')
+        const navLink = document.querySelector(`#${section.dataset.nav}`)
+        
+        if ((0 - rect.height) <= rect.top && rect.top < 0) {
+            navLink.classList.add('active')
+        } else {
+            navLink.classList.remove('active')
+        }
+    })
 })
+
