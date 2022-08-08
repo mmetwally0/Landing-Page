1aa07ba451367d32c79112bcba471b53583b938d
mmetwally0 <mohamed.saleh_a1208@pt.kfs.edu.eg>
2022-08-08 17:23:10 +0200

    Second submission

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
-#LANDING PAGE PROJECT FOR UDACITY NANODEGREE PROGRAM
+
+# Landing Page Project For Udacity Nanodegree Program
+
+## Table of contents
+- Description
+- Development
+- Features
+- Contributors
+
+## Description
+This project is a demo landing page, which mainly focuses on using Js to manipulate the DOM, and add navigation items dynamically.
+
+## Development
+This project was built using the following languages
+1. HTML5
+2. CSS3 (using SASS)
+3. Vanilla Javascript
+
+## Features
+1. The navigation bar Sections are added dynamically using JS.
+2. The Section which is currently in the view port is showed as active in the Navigation bar.
+3. A scroll to top button that appears only when pages are scrolled.
+4. Navigation bar hides while not scrolling
+5. On smaller screen sizes, a navigation menu appears with a hamburger menu that triggers it.
+
+# Contributors
+Mohamed Saleh
--- js/app.js
+++ js/app.js
@@ -10,14 +10,14 @@ const fragment = document.createDocumentFragment();
 
 // Looping through each of the sections
 sections.forEach((section) => {
-  // Creating a new li Element and setting its contents to the data-set from section.
-  const li = document.createElement("li");
-  li.textContent = `Section ${section.dataset.nav}`;
-  li.setAttribute("id", `${section.dataset.nav}`);
-  li.classList.add("nav-link");
+    // Creating a new li Element and setting its contents to the data-set from section.
+    const li = document.createElement("li");
+    li.textContent = `Section ${section.dataset.nav}`;
+    li.setAttribute("id", `${section.dataset.nav}`);
+    li.classList.add("nav-link");
 
-  // Adding the Element to the fragment
-  fragment.appendChild(li);
+    // Adding the Element to the fragment
+    fragment.appendChild(li);
 });
 
 // Adding the Navigation links to the DOM
@@ -25,61 +25,62 @@ navLinks.appendChild(fragment);
 
 // Creating a click event listener
 navLinks.addEventListener("click", (event) => {
-  const clickedLink = `section-${event.target.id}`;
-  event.preventDefault();
-  
-  // Scroll to the desired section
-  const element = document.querySelector(`#${clickedLink}`);
-  
-  element.scrollIntoView();
+    const clickedLink = `section-${event.target.id}`;
+    event.preventDefault();
+
+    // Scroll to the desired section
+    const element = document.querySelector(`#${clickedLink}`);
+
+    element.scrollIntoView({
+        behavior: 'smooth'
+    });
 });
 
 // Creating a scroll event listener
 document.addEventListener("scroll", (event) => {
-  // Loop Through Every section on page
-  sections.forEach((section) => {
-    const rect = section.getBoundingClientRect();
-    const navLinks = document.querySelectorAll("nav-link");
-    const navLink = document.querySelector(`#${section.dataset.nav}`);
+    // Loop Through Every section on page
+    sections.forEach((section) => {
+        const rect = section.getBoundingClientRect();
+        const navLink = document.querySelector(`#${section.dataset.nav}`);
 
-    // Check if the section is in viewPort
-    if (0 - rect.height <= rect.top && rect.top < 0) {
-      navLink.classList.add("active");
+        // Check if the section is in viewPort
+        if (10 - rect.height <= rect.top && rect.top < 10) {
+            navLink.classList.add("active");
+        } else {
+            navLink.classList.remove("active");
+        }
+    });
+
+    // Show nav Bar on scroll
+    nav.classList.remove("hide");
+
+    // Hide Nav Bar when not scrolling
+    let prevScroll = this.scrollY;
+
+    setTimeout(() => {
+        const currentScroll = this.scrollY;
+        if (currentScroll === prevScroll && this.scrollY != 0 && !(navLinks.classList.contains('open'))) {
+            nav.classList.add("hide");
+        }
+    }, 1000);
+
+    // Show Scroll to top button if one page scrolled
+    if (this.scrollY >= window.innerHeight) {
+        btnScrollToTop.classList.add('scroll-btn-visible')
     } else {
-      navLink.classList.remove("active");
+        btnScrollToTop.classList.remove('scroll-btn-visible')
     }
-  });
-
-  // Show nav Bar on scroll
-  nav.classList.remove("hide");
-
-  // Hide Nav Bar when not scrolling
-  let prevScroll = this.scrollY;
-
-  setTimeout(() => {
-    const currentScroll = this.scrollY;
-    if (currentScroll === prevScroll && this.scrollY != 0 && !(navLinks.classList.contains('open'))) {
-      nav.classList.add("hide");
-    }
-  }, 1000);
-
-  // Show Scroll to top button if one page scrolled
-  if (this.scrollY >= window.innerHeight) {
-    btnScrollToTop.classList.add('scroll-btn-visible')
-} else {
-    btnScrollToTop.classList.remove('scroll-btn-visible')
-  }
 });
 
 // Add menu Button animation and function
 menuBtn.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  menuBtn.classList.toggle("animate");
+    navLinks.classList.toggle("open");
+    menuBtn.classList.toggle("animate");
 });
 
 // Show nav Bar If the page is scrolled to the top
 if (window.scrollY === 0) {
-  nav.classList.remove("hide")
+    nav.classList.remove("hide")
 };
 
 // Scroll To Top
