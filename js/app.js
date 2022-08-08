0af5e18193d09d112cb92027a42588684cc88690
mmetwally0 <mohamed.saleh_a1208@pt.kfs.edu.eg>
2022-08-08 11:39:17 +0200

    First Submission

--- js/app.js
+++ js/app.js
@@ -1,73 +1,88 @@
 // Select Elements from the DOM
-const sections = document.querySelectorAll('section');
-const navLinks = document.querySelector('.navigation-links');
-
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelector(".navigation-links");
+const menuBtn = document.querySelector(".menu-btn");
+const nav = document.querySelector("nav");
+const btnScrollToTop = document.querySelector('#btnScrollToTop');
 
 // Create A document fragment to store the 'li' elements before appending them to the DOM
 const fragment = document.createDocumentFragment();
 
-
 // Looping through each of the sections
 sections.forEach((section) => {
-    // Creating a new li Element and setting its contents to the data-set from section.
-    const li = document.createElement('li');
-    li.textContent = `Section ${section.dataset.nav}`;
-    li.setAttribute('id', `${section.dataset.nav}`)
-    li.classList.add('nav-link')
+  // Creating a new li Element and setting its contents to the data-set from section.
+  const li = document.createElement("li");
+  li.textContent = `Section ${section.dataset.nav}`;
+  li.setAttribute("id", `${section.dataset.nav}`);
+  li.classList.add("nav-link");
 
-    // Adding the Element to the fragment
-    fragment.appendChild(li);
+  // Adding the Element to the fragment
+  fragment.appendChild(li);
 });
 
 // Adding the Navigation links to the DOM
 navLinks.appendChild(fragment);
 
 // Creating a click event listener
-navLinks.addEventListener('click', (event) => {
-    const clickedLink = `section-${event.target.id}`
-    event.preventDefault()
-    
-    // Scroll to the desired section
-    const element = document.querySelector(`#${clickedLink}`)
-    
-    element.scrollIntoView()
-})
+navLinks.addEventListener("click", (event) => {
+  const clickedLink = `section-${event.target.id}`;
+  event.preventDefault();
+  
+  // Scroll to the desired section
+  const element = document.querySelector(`#${clickedLink}`);
+  
+  element.scrollIntoView();
+});
 
 // Creating a scroll event listener
-document.addEventListener('scroll', (event) => {
-    // // use inner height and this.scrollY to get the height scrolled.
-    // const section = document.querySelector('#section-one')
-    // const rect = section.getBoundingClientRect()
-    // const link = document.querySelector('#one')
-    // // console.log(rect.top)
-    // // console.log(rect.bottom)
-    
-    // if ((0 - rect.height) <= rect.top && rect.top < 1) {
-    //     link.classList.add('active')
-    // } else {
-        
-    //     link.classList.remove('active')
-    // }
-    //---------------------------------
+document.addEventListener("scroll", (event) => {
+  // Loop Through Every section on page
+  sections.forEach((section) => {
+    const rect = section.getBoundingClientRect();
+    const navLinks = document.querySelectorAll("nav-link");
+    const navLink = document.querySelector(`#${section.dataset.nav}`);
 
-    // Loop Through Every section on page
-    sections.forEach((section) => {
-        const rect = section.getBoundingClientRect();
-        const navLinks = document.querySelectorAll('nav-link')
-        const navLink = document.querySelector(`#${section.dataset.nav}`)
-        
-        if ((0 - rect.height) <= rect.top && rect.top < 0) {
-            navLink.classList.add('active')
-        } else {
-            navLink.classList.remove('active')
-        }
-    })
-})
+    // Check if the section is in viewPort
+    if (0 - rect.height <= rect.top && rect.top < 0) {
+      navLink.classList.add("active");
+    } else {
+      navLink.classList.remove("active");
+    }
+  });
 
-const btn = document.querySelector('.menu-btn')
+  // Show nav Bar on scroll
+  nav.classList.remove("hide");
 
-btn.addEventListener('click', () => {
-    navLinks.classList.toggle('open');
-    btn.classList.toggle('animate');
+  // Hide Nav Bar when not scrolling
+  let prevScroll = this.scrollY;
 
+  setTimeout(() => {
+    const currentScroll = this.scrollY;
+    if (currentScroll === prevScroll && this.scrollY != 0 && !(navLinks.classList.contains('open'))) {
+      nav.classList.add("hide");
+    }
+  }, 1000);
+
+  // Show Scroll to top button if one page scrolled
+  if (this.scrollY >= window.innerHeight) {
+    btnScrollToTop.classList.add('scroll-btn-visible')
+} else {
+    btnScrollToTop.classList.remove('scroll-btn-visible')
+  }
+});
+
+// Add menu Button animation and function
+menuBtn.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+  menuBtn.classList.toggle("animate");
+});
+
+// Show nav Bar If the page is scrolled to the top
+if (window.scrollY === 0) {
+  nav.classList.remove("hide")
+};
+
+// Scroll To Top
+btnScrollToTop.addEventListener('click', () => {
+    window.scrollTo(0, 0);
 })
